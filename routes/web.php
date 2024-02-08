8a6cd262eac2ffca54cc7f0fc6bfffae7430cb01
Angela <antunezangela89@gmail.com>
2024-02-08 23:11:01 +0100

    Add correct routes and clean up the file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,67 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::controller(PageController::class)->group(function () {
 
-// Route::get('test', function () {
-//     return 'Welcome coders!';
-// });
+    Route::get('/', 'home')->name('home');
 
-// Route::get('blog', function () {
-//     return view('blog');
-// });
+    Route::get('/blog', 'blog')->name('blog');
 
-// Route::get('hello-world', function () {
-//     return view('hello-world');
-// });
-
-// Route::get('holi', function () {
-//     return view('holi');
-// });
-
-Route::get('/', function () {
-    return view('home');
-}) -> name ('home');
-
-
-
-Route::get('/blog', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'titulo' => 'Hoy en nutriciones Loli',
-            'contenido' => 'Lorem ipsum dolerem bla bla bla',
-        ],
-        [
-            'id' => 2,
-            'titulo' => 'Como evitar dormirte en clase',
-            'contenido' => 'recuerda que comer espinacas es importante bla bla bla',
-        ]
-    ];
-    return view('blog', ['posts'=> $posts]);
-})-> name('blog');
-
-Route::get('blog/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'titulo' => 'Hoy en nutriciones Loli',
-            'contenido' => 'Lorem ipsum dolerem bla bla bla',
-        ],
-        [
-            'id' => 2,
-            'titulo' => 'Como evitar dormirte en clase',
-            'contenido' => 'recuerda que comer espinacas es importante bla bla bla',
-        ]
-    ];
-    $selectedPost = null;
-    foreach($posts as $post){
-        if ($post['id']== $slug){
-            $selectedPost = $post;
-        }
-    }
-
-    return view('post', ['post' => $selectedPost]);
-}) -> name('post');
+    Route::get('blog/{slug}', 'post')->name('post');
+});
