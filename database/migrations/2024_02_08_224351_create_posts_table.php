e099c8976cb38463e5919a05637d541120ea7236
Angela <antunezangela89@gmail.com>
2024-02-09 01:22:59 +0100

    Correct typo (it's posts, not post)

--- database/migrations/2024_02_08_224351_create_posts_table.php
+++ database/migrations/2024_02_08_224351_create_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
             $table->string('contenido');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('posts');
     }
 };
