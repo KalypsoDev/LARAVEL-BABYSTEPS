a659cfb47f7db5a4ce84acac9ae8f4d2edfef59b
Angela <antunezangela89@gmail.com>
2024-02-11 03:23:48 +0100

    Update PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,8 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'titulo' => $this->faker->sentence(),
-            'contenido' => $this->faker->text(100)
+            'titulo' => $title = $this->faker->sentence(),
+            'contenido' => $this->faker->text(100),
+            'id_user' => User::all()->random()->id
         ];
     }
 }
